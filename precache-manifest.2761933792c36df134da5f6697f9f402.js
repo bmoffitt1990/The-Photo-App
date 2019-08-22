93ace5c5a51f595ad2b57058f5b0573400b5ccea
bmoffitt1990 <bmoffitt1990@gmail.com>
2019-08-22 14:17:31 -0400

    Updates

---
+++ precache-manifest.2761933792c36df134da5f6697f9f402.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05748c0e70f84305df0f529df7660eb3",
+    "url": "/The-Photo-App/index.html"
+  },
+  {
+    "revision": "8a598c846b9d841eaf49",
+    "url": "/The-Photo-App/static/css/main.43b134e8.chunk.css"
+  },
+  {
+    "revision": "82d7b7ecbd4383f8a2ee",
+    "url": "/The-Photo-App/static/js/2.ce44b6ea.chunk.js"
+  },
+  {
+    "revision": "8a598c846b9d841eaf49",
+    "url": "/The-Photo-App/static/js/main.6ae6eda1.chunk.js"
+  },
+  {
+    "revision": "2d3742d3c82f2d427b24",
+    "url": "/The-Photo-App/static/js/runtime~main.73d64971.js"
+  }
+]);
